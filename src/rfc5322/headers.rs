06823463b08e6e156c3990e9b95fa79b4d60345f
Mike Dilger <mike@optcomp.nz>
2016-09-21 11:24:48 +1200

    rfc5322: req_name! macro

--- src/rfc5322/headers.rs
+++ src/rfc5322/headers.rs
@@ -1,1 +1,10 @@
 
+macro_rules! req_name {
+    ($rem:ident, $str:expr, $input:ident) => {
+        let len: usize = $str.len();
+        if $rem.len() < len || &(&$rem[0..len]).to_ascii_lowercase()!=$str {
+            return Err(ParseError::NotFound);
+        }
+        $rem = &$rem[len..];
+    };
+}

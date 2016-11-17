9954a659eed649ebd4eccf87039588123867d6b2
Mike Dilger <mike@optcomp.nz>
2016-11-17 13:41:40 +1300

    TryFrom trait

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,25 @@ extern crate buf_read_ext;
 mod tests;
 
 pub mod rfc5322;
+
+/// Attempt to construct `Self` via a conversion.
+///
+/// This TryFrom trait is defined in the rust std library but is behind a
+/// feature gate.  We place it here so that people using stable compilers
+/// can still use our crate.  In the future, the std trait should be used.
+pub trait TryFrom<T>: Sized {
+    /// The type returned in the event of a conversion error.
+    type Err;
+
+    /// Performs the conversion.
+    fn try_from(T) -> Result<Self, Self::Err>;
+}
+
+// We implement TryFrom from T to T with our ParseError for crate ergonomics
+// (Rust won't let it be implemented with an unconstrained error type)
+impl<T> TryFrom<T> for T {
+    type Err = ::rfc5322::error::ParseError;
+    fn try_from(input: T) -> Result<T, Self::Err> {
+        Ok(input)
+    }
+}

563fd2a2769c39636d6542287f5014b0b1218882
Mike Dilger <mike@optcomp.nz>
2017-10-13 09:55:24 +1300

    Documentation updated

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["email_format"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='email_format/rfc5322/types/struct.QuotedPair.html' title='email_format::rfc5322::types::QuotedPair'>QuotedPair</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='email_format/rfc5322/types/struct.FWS.html' title='email_format::rfc5322::types::FWS'>FWS</a>",];
+implementors["email_format"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"email_format/rfc5322/types/struct.QuotedPair.html\" title=\"struct email_format::rfc5322::types::QuotedPair\">QuotedPair</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"email_format/rfc5322/types/struct.FWS.html\" title=\"struct email_format::rfc5322::types::FWS\">FWS</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["email_format"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='email_format/rfc5322/error/enum.ParseError.html' title='email_format::rfc5322::error::ParseError'>ParseError</a>",];
+implementors["email_format"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">StdError</a> for <a class=\"enum\" href=\"email_format/rfc5322/error/enum.ParseError.html\" title=\"enum email_format::rfc5322::error::ParseError\">ParseError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
